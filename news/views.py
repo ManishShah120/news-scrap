73d6f583f43665521d8aeeb0e4428b6273372284
Manish Kumar Shah <mkshah141@gmail.com>
2020-08-07 15:01:36 +0530

    Added search query

--- news/views.py
+++ news/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
 from .models import News
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 def news_list(request):
-    queryset = News.objects.all()
+    queryset = News.objects.all()   #Getting all the objects from the database
 
+    search_query = request.GET.get('q')
+    if search_query:
+        queryset = queryset.filter(
+            Q(title__icontains = search_query) |
+            Q(description__icontains = search_query)
+        )
 
-    paginator = Paginator(queryset, 5)
+    paginator = Paginator(queryset, 5)  #Adding pagination
     page_number = request.GET.get('page')
     queryset = paginator.get_page(page_number)
-
 
     context = {
        'object_list': queryset
     }
+
     return render(request, 'news_list.html', context)
 
 
